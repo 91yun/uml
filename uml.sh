8ec0f8daa99a56879f67755848b25d18f4e8abab
DESKTOP-08H5V75\22589 <rongdede@sina.com>
2017-03-15 01:09:29 +0800

    0.2

--- uml.sh
+++ uml.sh
@@ -48,6 +48,8 @@ tar zfvx uml-ssr-64.tar.gz
 cd uml-ssr-64
 cur_dir=`pwd`
 cat > run.sh<<-EOF
+#!/bin/sh
+export HOME=/root
 tunctl -t tap1
 ifconfig tap1 10.0.0.1
 ifconfig tap1 up
@@ -58,7 +60,6 @@ iptables -I FORWARD -i tap1 -j ACCEPT
 iptables -I FORWARD -o tap1 -j ACCEPT
 iptables -t nat -A PREROUTING -i venet0 -p tcp --dport 9191 -j DNAT --to-destination 10.0.0.2
 iptables -t nat -A PREROUTING -i venet0 -p udp --dport 9191 -j DNAT --to-destination 10.0.0.2
-
 nohup ${cur_dir}/vmlinux ubda=${cur_dir}/alpine-x64 eth0=tuntap,tap1 mem=64m & disown
 sleep 1
 ps aux | grep "vmlinux"
@@ -73,5 +74,69 @@ fi
 
 EOF
 chmod +x run.sh
-bash run.sh
+
+
+
+# Add run on system start up
+cat > /etc/init.d/uml<<-EOF
+### BEGIN INIT INFO
+# Provides: uml
+# Required-Start: $network $syslog
+# Required-Stop: $network
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Description: Start or stop the uml
+### END INIT INFO
+
+
+name=uml
+
+start(){
+	bash ${cur_dir}/run.sh
+}
+
+stop(){
+    kill \$( ps aux | grep vmlinux )
+	ifconfig tap1 down
+}
+
+status(){
+pid=\$(screen -list | grep pts | awk '{print \$1}')
+if [ "\$pid" == "" ]; then
+	screen /dev/pts/1
+else
+	screen -r \$(screen -list | grep pts | awk 'NR==1{print \$1}')
+fi	
+}
+
+case "\$1" in
+'start')
+    start
+    ;;
+'stop')
+    stop
+    ;;
+'status')
+    status
+    ;;
+'restart')
+    stop
+    start
+    ;;
+*)
+    echo "Usage: \$0 { start | stop | restart | status }"
+    ;;
+esac
+exit
+EOF
+
+chmod +x /etc/init.d/uml
+if [ "$OS" == 'CentOS' ]; then
+	chkconfig --add uml
+	chkconfig uml on
+else
+	update-rc.d -f uml defaults
+fi
+# Run ShadowsocksR in the background
+/etc/init.d/uml start
 
