990de583e8d9eb7cb6d92158afbef17e6c6a42c0
DESKTOP-08H5V75\22589 <rongdede@sina.com>
2017-03-15 01:21:31 +0800

    no message

--- uml.sh
+++ uml.sh
@@ -102,7 +102,7 @@ stop(){
 
 status(){
 pid=\$(screen -list | grep pts | awk '{print \$1}')
-if [ "\$pid" == "" ]; then
+if [ ! -n "\$pid" ]; then
 	screen /dev/pts/1
 else
 	screen -r \$(screen -list | grep pts | awk 'NR==1{print \$1}')
