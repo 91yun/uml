3028b1c8d185e84af6ce4c3b9a20c948853508bf
DESKTOP-08H5V75\22589 <rongdede@sina.com>
2017-03-15 18:05:01 +0800

    no message

---
+++ bbr/centos6.sh
@@ -0,0 +1,112 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#=================================================================#
+#   System Required:  CentOS 6                                    #
+#   Description: One click Install UML for bbr+ssr                #
+#   Author: 91yun <https://twitter.com/91yun>                     #
+#   Thanks: @allient                                              #
+#   Intro:  https://www.91yun.org                                 #
+#=================================================================#
+
+
+
+
+yum install -y tunctl uml-utilities screen
+
+
+wget http://soft.91yun.org/uml/91yun/uml-ssr-64.tar.gz
+tar zfvx uml-ssr-64.tar.gz
+cd uml-ssr-64
+cur_dir=`pwd`
+cat > run.sh<<-EOF
+#!/bin/sh
+export HOME=/root
+tunctl -t tap1
+ifconfig tap1 10.0.0.1
+ifconfig tap1 up
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -P FORWARD ACCEPT 
+iptables -t nat -A POSTROUTING -o venet0 -j MASQUERADE
+iptables -I FORWARD -i tap1 -j ACCEPT
+iptables -I FORWARD -o tap1 -j ACCEPT
+iptables -t nat -A PREROUTING -i venet0 -p tcp --dport 9191 -j DNAT --to-destination 10.0.0.2
+iptables -t nat -A PREROUTING -i venet0 -p udp --dport 9191 -j DNAT --to-destination 10.0.0.2
+nohup ${cur_dir}/vmlinux ubda=${cur_dir}/alpine-x64 eth0=tuntap,tap1 mem=64m &
+sleep 1
+ps aux | grep "vmlinux"
+if [ $? -eq 0 ]; then
+	echo "all things done!"
+	echo "you can use command to login uml:"
+	echo "/etc/init.d/uml status"
+	echo "user:root password:root"
+else
+	echo "some things error"
+fi	
+
+EOF
+chmod +x run.sh
+
+
+
+# Add run on system start up
+cat > /etc/init.d/uml<<-EOF
+### BEGIN INIT INFO
+# Provides:          uml
+# Required-Start:    \$syslog \$network \$local_fs \$remote_fs 
+# Required-Stop:     \$syslog \$network \$local_fs \$remote_fs 
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: uml
+# Description:       uml.
+### END INIT INFO
+
+
+name=uml
+
+start(){
+	bash ${cur_dir}/run.sh
+}
+
+stop(){
+    kill \$( ps aux | grep vmlinux )
+	ifconfig tap1 down
+}
+
+status(){
+pid=\$(screen -list | grep pts | awk '{print \$1}')
+if [ "\$pid" == "" ]; then
+	screen /dev/pts/1
+else
+	screen -r \$(screen -list | grep pts | awk 'NR==1{print \$1}')
+fi	
+}
+
+case "\$1" in
+'start')
+    start
+    ;;
+'stop')
+    stop
+    ;;
+'status')
+    status
+    ;;
+'restart')
+    stop
+    start
+    ;;
+*)
+    echo "Usage: \$0 { start | stop | restart | status }"
+    ;;
+esac
+exit
+EOF
+
+
+chmod +x /etc/init.d/uml
+chkconfig --add uml
+chkconfig uml on
+
+/etc/init.d/uml start
+
---
+++ bbr/uml.sh
@@ -0,0 +1,71 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#=================================================================#
+#   System Required:  CentOS 6,7, Debian, Ubuntu                  #
+#   Description: One click Install UML for bbr+ssr                #
+#   Author: 91yun <https://twitter.com/91yun>                     #
+#   Thanks: @allient                                              #
+#   Intro:  https://www.91yun.org/archives/2079                   #
+#=================================================================#
+
+
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "Error:This script must be run as root!" 1>&2
+   exit 1
+fi
+
+
+if [ -f /etc/redhat-release ]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    echo "Not support OS, Please reinstall OS and retry!"
+    exit 1	
+fi
+
+function getversion(){
+    if [[ -s /etc/redhat-release ]];then
+        grep -oE  "[0-9.]+" /etc/redhat-release
+    else    
+        grep -oE  "[0-9.]+" /etc/issue
+    fi    
+}
+
+centosversion() {
+    if [ "${release}" == "centos" ]; then
+        local code=$1
+        local version="$(getversion)"
+        local main_ver=${version%%.*}
+        if [ "$main_ver" == "$code" ]; then
+            return 0
+        else
+            return 1
+        fi
+    else
+        return 1
+    fi
+}
+
+if [[ "$release" = "centos" && "centosversion" = "6" ]]; then
+	wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/uml/master/bbr/centos6.sh && bash centos6.sh
+fi	
+
+
+
+
+
+
