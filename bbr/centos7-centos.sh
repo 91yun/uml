e1cdcbae61478cda1231ea34e5a3980cafd13937
DESKTOP-08H5V75\22589 <rongdede@sina.com>
2017-03-20 22:53:30 +0800

    no message

---
+++ bbr/centos7-centos.sh
@@ -0,0 +1,90 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#=================================================================#
+#   System Required:  CentOS 7                                    #
+#   Description: One click Install UML for bbr+ssr                #
+#   Author: 91yun <https://twitter.com/91yun>                     #
+#   Thanks: @allient neko @Jacky Bao                              #
+#   Intro:  https://www.91yun.org                                 #
+#=================================================================#
+
+
+
+
+yum install -y tunctl uml-utilities screen
+
+
+wget http://soft.91yun.org/uml/91yun/uml-centos-64.tar.gz
+tar zfvx uml-centos-64.tar.gz
+cd uml-centos-64
+cur_dir=`pwd`
+cat > run.sh<<-EOF
+#!/bin/sh
+export HOME=/root
+start(){
+	ip tuntap add tap1 mode tap 
+	ip addr add 10.0.0.1/24 dev tap1 
+	ip link set tap1 up 
+	echo 1 > /proc/sys/net/ipv4/ip_forward
+	iptables -P FORWARD ACCEPT 
+	iptables -t nat -A POSTROUTING -o venet0 -j MASQUERADE
+	iptables -I FORWARD -i tap1 -j ACCEPT
+	iptables -I FORWARD -o tap1 -j ACCEPT
+	iptables -t nat -A PREROUTING -i venet0 -p tcp --dport 9191 -j DNAT --to-destination 10.0.0.2
+	iptables -t nat -A PREROUTING -i venet0 -p udp --dport 9191 -j DNAT --to-destination 10.0.0.2
+	screen -dmS uml ${cur_dir}/vmlinux ubda=${cur_dir}/CentOS_fs eth0=tuntap,tap1 mem=256m
+	ps aux | grep vmlinux
+}
+
+stop(){
+    kill \$( ps aux | grep vmlinux )
+	ifconfig tap1 down
+}
+
+status(){
+
+	screen -r \$(screen -list | grep uml | awk 'NR==1{print \$1}')
+	
+}
+action=\$1
+#[ -z \$1 ] && action=status
+case "\$action" in
+'start')
+    start
+    ;;
+'stop')
+    stop
+    ;;
+'status')
+    status
+    ;;
+'restart')
+    stop
+    start
+    ;;
+*)
+    echo "Usage: \$0 { start | stop | restart | status }"
+    ;;
+esac
+exit
+EOF
+
+#创建和uml的共享目录
+mkdir -p /root/umlshare
+
+
+chmod +x run.sh
+bash run.sh start
+
+echo "/bin/bash ${cur_dir}/run.sh start" >> /etc/rc.d/rc.local
+sed -i "s/exit 0/ /ig" /etc/rc.local
+
+chmod +x /etc/rc.d/rc.local
+umlstatus=$(ps aux | grep vmlinux)
+if [ "$umlstatus" == "" ]; then
+	echo "some thing error!"
+else
+	echo "uml install success!"
+fi	
+
---
+++ bbr/uml-centos.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#=================================================================#
+#   System Required:  CentOS 6,7, Debian, Ubuntu                  #
+#   Description: One click Install UML for bbr+ssr                #
+#   Author: 91yun <https://twitter.com/91yun>                     #
+#   Thanks: @allient                                              #
+#   Intro:  https://www.91yun.org/archives/2079                   #
+#=================================================================#
+
+
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "Error:This script must be run as root!" 1>&2
+   exit 1
+fi
+
+
+if [ -f /etc/redhat-release ]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    echo "Not support OS, Please reinstall OS and retry!"
+    exit 1	
+fi
+
+function getversion(){
+    if [[ -s /etc/redhat-release ]];then
+        grep -oE  "[0-9.]+" /etc/redhat-release
+    else    
+        grep -oE  "[0-9.]+" /etc/issue
+    fi    
+}
+ver=""
+centosversion() {
+    if [ "${release}" == "centos" ]; then
+        local version="$(getversion)"
+        local main_ver=${version%%.*}
+		ver=$main_ver
+    else
+        return 1
+    fi
+}
+centosversion
+if [[ "$release" = "centos" && "$ver" = "6" ]]; then
+	wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/uml/master/bbr/centos6-centos.sh && bash centos6.sh
+elif [[ "$release" = "centos" && "$ver" = "7" ]]; then
+	wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/uml/master/bbr/centos7-centos.sh && bash centos7.sh
+elif [[ "$release" = "ubuntu" ]]; then
+	wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/uml/master/bbr/ubuntu-centos.sh && bash ubuntu.sh
+elif [[ "$release" = "debian" ]]; then
+	wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/uml/master/bbr/ubuntu-centos.sh && bash ubuntu.sh	
+fi	
+
+
+
+
+
+
